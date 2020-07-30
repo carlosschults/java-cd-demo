8c4286eae210a2c09b2b5e5a7c9ce6142d46c4bb
Carlos Schults <carlos@carlosschults.net>
2020-07-30 08:35:08 -0300

    First commit

---
+++ src/test/java/com/carlosschults/learning/LearningApplicationTests.java
@@ -0,0 +1,13 @@
+package com.carlosschults.learning;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearningApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
